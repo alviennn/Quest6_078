eb7b8ace5d62f4f5545a88d95a8def9936750528
alviennn <alvien.rnp@gmail.com>
2024-11-20 17:23:09 +0700

    Menambahkan icon Notification dengan warna putih

--- app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -69,7 +72,13 @@ fun RencanaStudyView(
                 fontSize = 12.sp,
                 color = Color.White
             )
-            Box{}
+            Box{
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
         }
     }
 }
