bb41f3795ca10d5d191760cb478e668412a27593
alviennn <alvien.rnp@gmail.com>
2024-11-20 17:02:19 +0700

    Menambahkan variable DropDown untuk mengelola pilihan dropdown

---
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -0,0 +1,36 @@
+package com.example.navigasiwithdata.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiwithdata.R
+import com.example.navigasiwithdata.model.Mahasiswa
+import com.example.navigasiwithdata.model.RencanaStudy
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+
+}
