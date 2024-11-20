ffbf66a63e678f4c36168e6dc7be15d065e970a8
alviennn <alvien.rnp@gmail.com>
2024-11-21 00:10:02 +0700

    Menambahkan fungsi MahasiswaApp untuk mendukung navigasi dan pengelolaan logika utama aplikasi

--- app/src/main/java/com/example/navigasiwithdata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigasiwithdata/navigation/PengelolaHalaman.kt
@@ -1,8 +1,27 @@
 package com.example.navigasiwithdata.navigation
 
+import android.widget.MediaController
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasiwithdata.ui.viewmodel.MahasiswaViewModel
+import com.example.navigasiwithdata.ui.viewmodel.RencanaStudyViewModel
+
 enum class Halaman{
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
 }
