ac0e409e1aef45b3881a4d6cc82eec2877de6c00
alviennn <alvien.rnp@gmail.com>
2024-11-20 17:39:46 +0700

    Spacer untuk memberikan jarak antara konten sebanyak 8 dp

--- app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -104,6 +104,7 @@ fun RencanaStudyView(
                     fontSize = 12.sp,
                     fontWeight = FontWeight.Light
                 )
+                Spacer(modifier = Modifier.padding(8.dp))
 
             }
         }
