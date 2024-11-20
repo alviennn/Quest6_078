afdda6b47313a471664c475411209df2ee8755a9
alviennn <alvien.rnp@gmail.com>
2024-11-21 00:14:58 +0700

    Menambahkan komponen NavHost untuk mengatur navigasi antar halaman dengan startDestination pada halaman Splash

--- app/src/main/java/com/example/navigasiwithdata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigasiwithdata/navigation/PengelolaHalaman.kt
@@ -1,12 +1,14 @@
 package com.example.navigasiwithdata.navigation
 
 import android.widget.MediaController
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.navigasiwithdata.ui.viewmodel.MahasiswaViewModel
 import com.example.navigasiwithdata.ui.viewmodel.RencanaStudyViewModel
@@ -26,4 +28,11 @@ fun MahasiswaApp(
     navController: NavHostController = rememberNavController()
 ) {
     val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ) {
+
+    }
 }
